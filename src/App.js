0a02b93f55665ec952033ecaa3c871d1362a80ca
mijelpalcabello <miguel.castillo861999@gmail.com>
2021-12-31 12:16:07 -0500

    pacakge.json -> Agregando sweetalert y animate.css app.js -> Agregando animaciones y errores, también cambiando el video dependiendo si es de noche o día

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import nubes from './assets/video/cielo.mp4';
+import noche from './assets/video/cielo_noche.mp4';
 import axios from 'axios';
+import Swal from 'sweetalert2'
+
+const IS_DAY = 1;
+const IS_NIGHT = 0;
 
 export const WeatherApp = () =>{
   
   const [ ciudad, setCiudad ] = useState('Lima');
+  const [ pais, setPais ] = useState('')
   const [ option, setOption ] = useState('');
   const [ temp, setTemp ] = useState();
   const [ clouds, setClouds ] = useState();
   const [ sensacion, setSensacion ] = useState();
-  const [ presion, setPresion ] = useState();
   const [ humedad, setHumedad ] = useState();
+  const [ isDay, setIsDay ] = useState();
 
   const fetchWeather = async (ciudadFetch = 'Lima') => {
     try {
       const res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=15227df04da9468184f14958213012&q=${ciudadFetch}&aqi=no`);    
         
-      setHumedad(res.data.current.humidity)
       setTemp(res.data.current.temp_c)
+      setHumedad(res.data.current.humidity)
       setSensacion(res.data.current.feelslike_c)
       setClouds(res.data.current.condition.text)
-      setPresion(res.data.current.precip_in)
+      setIsDay(res.data.current.is_day)
+      setPais(res.data.location.country)
       
     }catch( e ) {
-      console.log( e )
+      Swal.fire('Ciudad no encontrada')
+      setCiudad('Lima')
     }
   }
 
@@ -53,13 +61,15 @@ export const WeatherApp = () =>{
       className="App"
     >    
       <div
+        className="animate__animated animate__bounceInDown"
         style={{
           color: 'white',
           paddingTop: '25px',
           paddingBottom: '25px'
         }}
+        key={ ciudad }
       >
-        { ciudad }
+        { ciudad }, { pais }
       </div>
       <div 
         style={{
@@ -67,14 +77,18 @@ export const WeatherApp = () =>{
         }}
       >
         <h1 
+          key={ temp }
+          className="animate__animated animate__bounceIn"
           style={{
             fontSize: '96px',
             color: 'white'
           }}
         >
-          { temp }°
+          { temp } C°
         </h1>
         <h1
+          key={ clouds }
+          className="animate__animated animate__bounceInLeft"
           style={{
             fontSize: '24px',
             color: 'white'
@@ -93,12 +107,20 @@ export const WeatherApp = () =>{
           fontWeight: 'bold'
         }}
       >
-        <h1 className="h6">Sensación térmica</h1> 
-        <p>{ sensacion }°</p>
+        <h1 
+          className="h6 animate__animated animate__bounceInLeft"
+        >
+          Sensación térmica
+        </h1> 
+        <p
+          key={ sensacion }
+          className="animate__animated animate__bounceInLeft"
+        >{ sensacion } °C</p>
       </div>
       
-
       <div 
+        className="animate__animated animate__bounceInLeft"
+        key={ isDay }
         style={{
           display: 'flex',
           justifyContent: 'space-between',
@@ -107,11 +129,21 @@ export const WeatherApp = () =>{
           fontWeight: 'bold'
         }}
       >
-        <h1 className="h6">Presión</h1> 
-        <p>{ presion }°</p>
+        <h1 className="h6">¿Día o Noche?</h1> 
+        
+          { 
+            ( isDay === IS_DAY )
+              ? (
+                <p className="animate__animated animate__bounceInLeft" key={ isDay }>Día</p>  
+              )
+              : (
+                <p className="animate__animated animate__bounceInLeft" >Noche</p>
+              )
+          }
       </div>
-
+      
       <div 
+        className="animate__animated animate__bounceInLeft"
         style={{
           display: 'flex',
           justifyContent: 'space-between',
@@ -121,16 +153,20 @@ export const WeatherApp = () =>{
         }}
       >
         <h1 className="h6">Humedad</h1> 
-        <p>{ humedad }°</p>
+        <p className="animate__animated animate__bounceInLeft" key={ humedad }>{ humedad } %</p>
       </div>
-
-      <div 
+      
+      <form 
+        onSubmit={(e) => {
+          e.preventDefault(); 
+        }}
         style={{
           width: '200px'
         }} 
         className="input-group"
       >
         <input 
+          autocomplete="off"
           type="text" 
           className="form-control"
           name="option"
@@ -143,28 +179,57 @@ export const WeatherApp = () =>{
         >
           O
         </button>
-      </div>
-      
-      <video 
-        id="background-video" 
-        style={{
-          height: '100%',
-          width: '100%',
-          float: 'left',
-          top: '0',
-          left: '0',
-          padding: 'none',
-          position: 'absolute',
-          objectFit: 'cover',
-          filter: 'blur(3px)',
-          zIndex: '-1',
-        }}
-        autoPlay
-        muted
-        loop
-      >
-        <source src={nubes} type="video/mp4"/>
-      </video>  
+      </form>
+
+      {
+          (isDay === IS_DAY)
+            ? (
+              <video 
+                className="animate__animated animate__fadeIn" 
+                key={ nubes }
+                style={{
+                  height: '100%',
+                  width: '100%',
+                  float: 'left',
+                  top: '0',
+                  left: '0',
+                  padding: 'none',
+                  position: 'absolute',
+                  objectFit: 'cover',
+                  filter: 'blur(3px)',
+                  zIndex: '-1',
+                }}
+              autoPlay
+              muted
+              loop
+              > 
+                <source src={ nubes } type="video/mp4"/> 
+            </video>
+            )
+            : (
+              <video 
+                className="animate__animated animate__fadeIn"
+                key={ noche }
+                style={{
+                  height: '100%',
+                  width: '100%',
+                  float: 'left',
+                  top: '0',
+                  left: '0',
+                padding: 'none',
+                position: 'absolute',
+                objectFit: 'cover',
+                filter: 'blur(3px)',
+                zIndex: '-1',
+                }}
+                autoPlay
+                muted
+                loop
+              > 
+                <source src={ noche } type="video/mp4"/> 
+            </video> 
+            )
+        }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { WeatherApp } from './App';
 
+import 'animate.css';
+
 ReactDOM.render(
     <WeatherApp />,
   document.getElementById('root')
